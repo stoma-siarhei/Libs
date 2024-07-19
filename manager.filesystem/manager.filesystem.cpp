5a560640961babc3910d5536cc9174c1d541adfb
scander <scander@yandex.ru>
2024-07-19 15:59:30 +0800

    #1 - Добавлен заголовочный файл

--- manager.filesystem/manager.filesystem.cpp
+++ manager.filesystem/manager.filesystem.cpp
@@ -1,1 +1,1 @@
-﻿#include "pch.h"
+﻿#include "manager.filesystem.h"
---
+++ manager.filesystem/manager.filesystem.h
@@ -0,0 +1,8 @@
+#ifndef _MANAGER_FILESYSTEM_H_
+#define _MANAGER_FILESYSTEM_H_
+
+
+#include "pch.h"
+
+
+#endif // _MANAGER_FILESYSTEM_H_
